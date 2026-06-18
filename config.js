622d66f9f212b3341cb2682400202bd0d5629eff
Shan-Boy...👻💗 <shanxboy2008@gmail.com>
2026-06-18 07:59:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'bRVH3IYQ#rvaA_gthFiBdz1mRrZ-q3dxCregiv93DQ0uKUQCYDls' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
